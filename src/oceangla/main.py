1d8526135ff6ecc14693e29f5a85dbefcbd0e97c
Joey Scanga <scangjo@gmail.com>
2026-05-21 14:30:32 -0500

    change default logging to level INFO

--- src/oceangla/main.py
+++ src/oceangla/main.py
@@ -16,10 +16,10 @@ import logging
 import sys
 
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
+handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
